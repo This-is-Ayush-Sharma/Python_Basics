867bed4c1cdbb314a4c8341555bf73bae38c073e
This-is-Ayush-Sharma <thisisayush79@gmail.com>
2023-03-13 16:28:42 +0530

    positional

---
+++ day1/categories.py
@@ -0,0 +1,11 @@
+# categories of function
+
+# based on arguments
+
+
+# Positional arguments
+def function(num1, num2, num3, num4):
+    print("num1", num1,"num2", num2,"num3", num3,"num4", num4)
+function(1,2,3,4)
+function(100,200,300) # error less parameter typeError
+function(1,2,3,4,5) # error extra parameter typeError
