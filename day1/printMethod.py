a7d6282f969a970feded925b7924010a0e2030e6
This-is-Ayush-Sharma <thisisayush79@gmail.com>
2023-03-13 15:10:36 +0530

    User input and print method decision making

---
+++ day1/printMethod.py
@@ -0,0 +1,26 @@
+# Python Programming
+
+
+#data Types
+
+
+# str
+name  = "Ayush Sharma"
+print("name", name)
+print("name", name, type(name))
+
+# int
+rollno = 12
+print("Roll",rollno,type(rollno))
+
+# float
+percentage = 23.3
+print("Percentage", percentage, type(percentage))
+
+# boolean
+student_y_no = True
+print("Are you a student", student_y_no, type(student_y_no))
+
+# complex Number
+complex_number = 3 + 7j
+print("Complex number",complex_number,type(complex_number))
