ecbb75fe2698658e5242d2b967cd7462274c082e
This-is-Ayush-Sharma <thisisayush79@gmail.com>
2023-03-13 15:39:21 +0530

    user os break statement

---
+++ day1/Control.py
@@ -0,0 +1,14 @@
+# control statement
+
+
+# break 
+
+
+# wap 1 to 100 print data less than 50 then break
+
+for i in range(1, 101):
+    if(i > 49):
+        break
+    print(i, end = ' ')
+
+print()
--- day1/looping.py
+++ day1/looping.py
@@ -46,3 +46,5 @@ for i in range(100,0,-2):
     print(i, end=' ')
 
 print()
+
+
