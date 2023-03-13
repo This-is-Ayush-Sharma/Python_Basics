7f3d767e8de25eeb6423a2c4b875296256bd020d
This-is-Ayush-Sharma <thisisayush79@gmail.com>
2023-03-13 15:48:31 +0530

    example of continue statement added

--- day1/Control.py
+++ day1/Control.py
@@ -5,10 +5,20 @@
 
 
 # wap 1 to 100 print data less than 50 then break
-
+print("Example of break statement")
 for i in range(1, 101):
     if(i > 49):
         break
+    print(i, end = ' ')
+
+print()
+
+
+# continue
+print("Example of continue statement")
+for i in range(1, 101):
+    if(i == 50):
+        continue
     print(i, end = ' ')
 
 print()
