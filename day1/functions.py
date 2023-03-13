613fba9632bef1edb28b5fad358521a9873f788d
This-is-Ayush-Sharma <thisisayush79@gmail.com>
2023-03-13 16:12:41 +0530

    few

--- day1/functions.py
+++ day1/functions.py
@@ -12,11 +12,29 @@ function1()
 
 # with parameters
 def function2(num1, num2):
-    print("num1",num1,"num2",num2)
+    print("num1:-",num1,"num2:-",num2)
 function2(10,30)
 
 
 # with parameters with return type
 def function3(num1, num2):
     return num1 + num2
-print("Value returned", function3(10,30))
+print("Value returned:-", function3(10,30))
+
+
+# with parameters with return type with int, float
+def function4(num1, num2):
+    return num1 + num2
+print("Value returned:-", function4(10, 20.3))
+
+# with parameters with return type with str, int
+def function5(num1, num2):
+    num1 = int(num1)
+    return num1 + num2
+print("Value returned:-", function5('10', 20))
+
+# with parameters with return type with str, int
+def function6(num1, num2):
+    num2 = str(num2)
+    return num1 + num2
+print("Value returned:-", function6('10', 20))
