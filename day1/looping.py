9a63279614ff6ea26b2d7b22d454d100f7531217
This-is-Ayush-Sharma <thisisayush79@gmail.com>
2023-03-13 15:32:33 +0530

    reverse order odd even in range 100 to 1

--- day1/looping.py
+++ day1/looping.py
@@ -31,3 +31,18 @@ print("Number from range 100 to 1")
 for i in range(100, 0, -1):
     print(i, end=' ')
 print()
+
+
+# odd number from 100 to 1 
+print("Number from range 100 to 1 Odd Number Reverse Order")
+for i in range(99,0,-2):
+    print(i,end=' ')
+
+print()
+
+# all even numbers from 1 to 100
+print("Number from range 100 to 1 Even Number Reverse Order")
+for i in range(100,0,-2):
+    print(i, end=' ')
+
+print()
