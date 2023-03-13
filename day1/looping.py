22307c7a590223ddb6d5b74d01b22a7ecd97522b
This-is-Ayush-Sharma <thisisayush79@gmail.com>
2023-03-13 15:23:04 +0530

    end parameter associated

--- day1/looping.py
+++ day1/looping.py
@@ -6,14 +6,20 @@
 # number from 1 to 100
 print("Number from range 1 to 100 all")
 for i in range(1, 101):
-    print(i)
+    print(i,end=' ')
+
+print()
 
 # odd number from 1 to 100
 print("Number from range 1 to 100 Odd Number")
 for i in range(1,101,2):
-    print(i)
+    print(i,end=' ')
+
+print()
 
 # all even numbers from 1 to 100
 print("Number from range 1 to 100 Even Number")
 for i in range(2,101,2):
-    print(i)
+    print(i, end=' ')
+
+print()
