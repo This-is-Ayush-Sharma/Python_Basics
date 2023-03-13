19d56748a41edd4fa9c156a353e5afeb4adf1916
This-is-Ayush-Sharma <thisisayush79@gmail.com>
2023-03-13 17:20:47 +0530

    default

--- day1/categories.py
+++ day1/categories.py
@@ -1,7 +1,9 @@
 # categories of function
 
-# based on arguments
 
+# values to the fuction is passed from right to left
+
+# based on arguments
 
 # Positional arguments
 def function(num1, num2, num3, num4):
@@ -11,8 +13,16 @@ function(1,2,3,4)
 # function(1,2,3,4,5) # error extra parameter typeError
 
 # Keyword arguments
-
 def function2(num1, num2, num3, num4):
     print("num1:-", num1,"num2:-", num2,"num3:-", num3,"num4:-", num4)
 
 function2(num4 = 10, num1 = 20, num2 = 30, num3 = 40)
+
+#default arguments
+
+# default arguments should be followed by non default arguments
+def function3(name , rollno, branch, collegename = "GIET"):
+    print(name , rollno, branch, collegename)
+
+function3("ayush", 11, "CSE")
+function3("Venu", 13, "CST")
