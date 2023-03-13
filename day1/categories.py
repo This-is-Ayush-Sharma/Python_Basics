bb0354f5757825d9c1fa81406ac38672cf3ae0d0
This-is-Ayush-Sharma <thisisayush79@gmail.com>
2023-03-13 16:34:39 +0530

    keywords

--- day1/categories.py
+++ day1/categories.py
@@ -5,7 +5,14 @@
 
 # Positional arguments
 def function(num1, num2, num3, num4):
-    print("num1", num1,"num2", num2,"num3", num3,"num4", num4)
+    print("num1:-", num1,"num2:-", num2,"num3:-", num3,"num4:-", num4)
 function(1,2,3,4)
-function(100,200,300) # error less parameter typeError
-function(1,2,3,4,5) # error extra parameter typeError
+# function(100,200,300) # error less parameter typeError
+# function(1,2,3,4,5) # error extra parameter typeError
+
+# Keyword arguments
+
+def function2(num1, num2, num3, num4):
+    print("num1:-", num1,"num2:-", num2,"num3:-", num3,"num4:-", num4)
+
+function2(num4 = 10, num1 = 20, num2 = 30, num3 = 40)
