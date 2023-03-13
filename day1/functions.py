6029fe288d45c320754b0c8ee28f7544010bd845
This-is-Ayush-Sharma <thisisayush79@gmail.com>
2023-03-13 16:01:40 +0530

    function

---
+++ day1/functions.py
@@ -0,0 +1,14 @@
+# functions
+
+# def <fn_name>()
+# def <fn_name>(<parameters>, <parameters>)
+
+# without parametres
+def function1():
+    print("Function 1")
+function1()
+
+# with parameters
+def function2(num1, num2):
+    print("num1",num1,"num2",num2)
+function2(10,30)
