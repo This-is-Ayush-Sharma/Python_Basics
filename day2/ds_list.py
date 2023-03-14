c0f6c4929bc0a661f5a61213de8009f9f25b870f
This-is-Ayush-Sharma <thisisayush79@gmail.com>
2023-03-14 14:24:06 +0530

    lists

--- day2/ds_list.py
+++ day2/ds_list.py
@@ -6,3 +6,15 @@
 # empty list 
 list1 = []
 print(list1, type(list1))
+
+#list with a integer values
+list2 = [10,20,30,40]
+print(list1, type(list2))
+
+#list with multiple types
+list3 = [10,3+7j,95.3,"ayush"]
+print(list3,type(list3))
+
+# list function
+list4 = list([1,2,3,4,5])
+print(list4,type(list4))
