c95c0a6bd1a521cd0ca67255557f050ca6c384b4
This-is-Ayush-Sharma <thisisayush79@gmail.com>
2023-03-15 16:09:51 +0530

    dictionary comprehension

--- day3/practice_QUESTION/question1.py
+++ day3/practice_QUESTION/question1.py
@@ -4,9 +4,9 @@ list_b = [6,4,6,1,2,2]
 # without list comprehension
 arr = []
 for i in list_b:
-    arr.append(tuple([i,mylist.index(i)]))
+    arr.append((i,mylist.index(i)))
 print(arr)
 
 # with list comprehension
-arr_data = [tuple([i,mylist.index(i)]) for i in list_b]
+arr_data = [(i,mylist.index(i)) for i in list_b]
 print(arr_data)
