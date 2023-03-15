095e09f14fb8e3d271c4ebcfa3fe24a0282ddc96
This-is-Ayush-Sharma <thisisayush79@gmail.com>
2023-03-15 14:49:06 +0530

    condition added inside list

--- day3/list_comp.py
+++ day3/list_comp.py
@@ -14,7 +14,6 @@ print(list_data)
 
 # add all odd elements
 odd_list = []
-sum=0
 for i in range(11):
     if(i%2==1):
         odd_list.append(i)
@@ -24,3 +23,16 @@ print(odd_list)
 odd_element_list = [int(i) for i in range(11) if(i%2==1)]
 print(odd_element_list)
 
+
+# add all odd number else do the sq and append
+combo_list = []
+for i in range(11):
+    if(i%2==1):
+        combo_list.append(i)
+    else:
+        combo_list.append(i**2)
+print(combo_list)
+
+# same using list comprehension
+combo_element_list = [int(i) if(i%2==1) else i**2 for i in range(11)]
+print(combo_element_list)
