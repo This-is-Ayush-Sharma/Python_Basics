63881e16491cb7405d55586452e1615517777158
This-is-Ayush-Sharma <thisisayush79@gmail.com>
2023-03-16 18:07:28 +0530

    oops concept

---
+++ day 4/Mobile.py
@@ -0,0 +1,11 @@
+class Mobile:
+    def __init__(self):
+        print(id(self))
+    def display(self):
+        print("Display details")
+    
+    def purchase(self):
+        self.display()
+        print("Calculating the price")
+
+Mobile().purchase()
