2a111fc19e307a8909f0b0ee4756e424d6a2299b
This-is-Ayush-Sharma <thisisayush79@gmail.com>
2023-03-31 19:08:16 +0530

    question done

--- data structure/day5/maxrep.py
+++ data structure/day5/maxrep.py
@@ -17,43 +17,40 @@ class LinkedList:
             while copy.next is not None:
                 copy = copy.next
             copy.next = node
-        
-    def logics(self, pos, slist_head):
-        c = 0
+
+    def get_max(self):
+        copy = self.head
+        maxv = -999
+        while copy is not None:
+            if(copy.data > maxv):
+                maxv = copy.data
+            copy = copy.next
+        return maxv
+    
+    def replace(self, new, maxv):
         copy = self.head
         while copy is not None:
-            c+=1
-            if(c == pos):
-                d = copy.next
-                copy.next = slist_head
-                while slist_head.next is not None:
-                    slist_head = slist_head.next
-                slist_head.next = d
+            if(copy.data == maxv):
+                copy.data = new
             copy = copy.next
-
+    
     def printList(self):
         copy = self.head
         while copy is not None:
-            print(copy.data,end=' ')
+            print(copy.data,end= ' ')
             copy = copy.next
         print()
 
 a = LinkedList()
-a.insert_At_end(1)
-a.insert_At_end(2)
-a.insert_At_end(4)
-a.insert_At_end(3)
-a.insert_At_end(5)
-print("First linked List")
+a.insert_At_end(12)
+a.insert_At_end(95)
+a.insert_At_end(140)
+a.insert_At_end(110)
+a.insert_At_end(40)
+
+print("Mav value is",a.get_max())
+print("Original list")
 a.printList()
-
-b = LinkedList()
-b.insert_At_end(9)
-b.insert_At_end(8)
-b.insert_At_end(11)
-print("Second Linked List")
-b.printList()
-
-a.logics(2,b.head)
-print("After merging")
+a.replace(100, a.get_max())
+print("after replacing max value with 100")
 a.printList()
